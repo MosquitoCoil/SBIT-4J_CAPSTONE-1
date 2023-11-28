ed1387d43694c4c566608c4ccb1ddc174b6248dd
Jaymar Roco <jaymar.roco@gmail.com>
2023-11-28 23:57:38 +0800

    login modal and registration modal

---
+++ .history/maternal_system/application/views/pages/home_view_20231128235717.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Maternology - Home</title>
+  
+  <!-- <link rel="shortcut icon" type="image/png" href="assets/images/logos/fav.png" />
+  <link rel="stylesheet" href="assets/css/styles.min.css" />
+  <link rel="stylesheet" href="assets/css/custom.css" />
+  <link rel="stylesheet" href="assets/css/fontawesome.css" /> -->
+  
+        <!-- CSS FILES -->        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400;600;700&display=swap" rel="stylesheet">
+        
+        <link rel="shortcut icon" type="image/png" href="assets/images/logos/fav.png" />
+
+        <link href="assets/hassets/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="assets/hassets/css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="assets/hassets/css/templatemo-ebook-landing.css" rel="stylesheet">
+        
+        <link rel="stylesheet" href="assets/css/fontawesome.css" />
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body class="">
+
+<style>
+    .bg-main{
+        background: rgb(17, 82, 114, 1);
+    }
+
+    .cust-border{
+        border-bottom: 1px solid lightgray;
+        width: 90%;
+        margin: auto;
+    }
+
+    .img-qcmc{
+        width: 710px;
+        height: 523px;
+        border-radius: 0px;
+        position: absolute;
+        float: right;
+    }
+
+    .bg-home-img{
+        background-image: url('assets/images/backgrounds/QC-city-hall.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-attachment: fixed;
+        background-position: center;
+        opacity: 0.5;
+        height:100%;
+        width: 100%;
+}
+
+/* You might need to set dimensions for .bg-home-img for the pseudo-element to work */
+
+</style>
+
+<main class=""> 
+
+    <nav class="navbar navbar-expand-lg pt-4">
+        <div class="container">
+            <a class="navbar-brand" href="home">
+            <img src="assets/images/logos/maternology.png" class="" width="75" alt="maternology logo" />
+                <span class="h5 text-dark"><b>Maternity System</b></span>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-lg-auto me-lg-1">
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" style="font-size: 80%; font-weight:lighter;" href=""><i class="fas fa-info"></i> About</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" style="font-size: 80%; font-weight:lighter;" href=""><i class="fas fa-envelope"></i> Contact</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" style="font-size: 80%; font-weight:lighter;" href="patient/login"><i class="fas fa-user-lock"></i> Login</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" style="font-size: 80%; font-weight:lighter;" href="patient/registration"><i class="fas fa-user-edit"></i> Register</a>                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    
+    <section class=" d-flex justify-content-center align-items-center" style="padding-top: 200px">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-12 mb-5 pb-5 pb-lg-0 mb-lg-0">
+                    <h1 class="text-dark mb-4">Maternal Health System of <br> District II <br><small class="h5">Developing Natural care in Motherhood</small></h1>
+                    <a href="" class="btn btn-dark shadow rounded-5 p-3">Get Started</a>
+                </div>
+
+                <div class="col-md-6 d-flex justify-content-center align-items-center">
+                    <!-- <img src="assets/images/backgrounds/qmc.png" class="img-qcmc" alt="QC" /> -->
+                </div>
+            </div>
+        </div>
+    </section>
+    
+
+</main>
+
+<!-- JAVASCRIPT FILES -->
+<script src="assets/hassets/js/jquery.min.js"></script>
+<script src="assets/hassets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/hassets/js/jquery.sticky.js"></script>
+<script src="assets/hassets/js/click-scroll.js"></script>
+<script src="assets/hassets/js/custom.js"></script>
+
+</body>
